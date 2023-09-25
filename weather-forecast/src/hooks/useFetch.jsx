0a532982d4c6a137361ac3e2503d8dba3c5f5ebb
guipmuller <guilhermepmuller@gmail.com>
2023-09-25 16:51:02 -0300

    Acessando informações da API

--- weather-forecast/src/hooks/useFetch.jsx
+++ weather-forecast/src/hooks/useFetch.jsx
@@ -1,21 +1,34 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import APIKey from "../helpers/APIKey";
 
-const useFetch = (url) => {
+const useFetch = () => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
- 
+
   useEffect(() => {
-    fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-        setError(data.error)
-        setData(data.name)
-        setLoading(false)
-    })
-  }, [url]);
- 
+    navigator.geolocation.getCurrentPosition((location) => {
+      
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${APIKey}`;
+      
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          setError(data.error)
+          setData(data)
+          setLoading(false)
+        })
+        .catch((error) => {
+          setError(error.message);
+          setLoading(false);
+        });
+    }, (error) => {
+      setError(error.message);
+      setLoading(false);
+    });
+  }, []);
+
   return { data, loading, error };
 };
- 
-export default useFetch
+
+export default useFetch;
--- weather-forecast/src/pages/home/Home.jsx
+++ weather-forecast/src/pages/home/Home.jsx
@@ -1,16 +1,26 @@
 import useFetch from "../../hooks/useFetch";
-import APIKey from "../../helpers/APIKey";
+import TempConverter from "../../helpers/TempConverter";
 
 const Home = () => {
-//   const url = "https://sv443.net/jokeapi/v2/joke/Programming?type=single";
-console.log(APIKey)
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${APIKey}`;
-  const { data, loading, error } = useFetch(url);
+  const { data, loading, error } = useFetch();
   if (loading) return <div>Loading...</div>;
   return (
     <div>
       {error && <div>{error}</div>}
-      {data && <div>{<div>{data}</div>}</div>}
+      {data && (
+        <div>
+          {
+            <div>
+              <p>Nome: {data.name}</p>
+              <p>Temp: {TempConverter(data.main.temp)}°C</p>
+              <p>Temp Max: {TempConverter(data.main.temp_max)}°C</p>
+              <p>Temp Min: {TempConverter(data.main.temp_min)}°C</p>
+              <p>Umidade: {data.main.humidity}</p>
+              <p>Clima: {data.weather[0].description}</p>
+            </div>
+          }
+        </div>
+      )}
     </div>
   );
 };
